3916e79e5c42856c9581c01038abd35a861e2bd8
Bhavik-20 <59500727+Bhavik-20@users.noreply.github.com>
2022-01-21 19:30:34 +0530

    Error printing on Login, Signup done

--- FNQ/accounts/views.py
+++ FNQ/accounts/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.test import tag
 
 
 def signup(request):
@@ -12,40 +13,54 @@ def signup(request):
         uname = request.POST['uname']
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
-
+        
         if fname == "":
-            messages.info(request, "Please enter your First name")
-            return redirect("signup")
+            # messages.info(request, "Please enter your First name")
+            # return redirect("signup")
+            context = { "fname" : "Please enter your First name", "lname" : "", "email": "", "uname": "", "pass": ""}
+            return render(request, "signup.html", context)
         elif lname == "":
-            messages.info(request, "Please enter your Last name")
-            return redirect("signup")
+            # messages.info(request, "Please enter your Last name")
+            # return redirect("signup")
+            context = { "fname" : "", "lname" : "Please enter your Last name", "email": "", "uname": "", "pass": ""}
+            return render(request, "signup.html", context)
         elif email == "":
-            messages.info(request, "Please enter your Email id")
-            return redirect("signup") 
+            # messages.info(request, "Please enter your Email id")
+            # return redirect("signup") 
+            context = { "fname" : "", "lname" : "", "email": "Please enter your Email id", "uname": "", "pass": ""}
+            return render(request, "signup.html", context)
         elif uname == "":
-            messages.info(request, "Please enter a Username")
-            return redirect("signup") 
+            # messages.info(request, "Please enter a Username")
+            # return redirect("signup") 
+            context = { "fname" : "", "lname" : "", "email": "", "uname": "Please enter a Username", "pass": ""}
+            return render(request, "signup.html", context)
         elif pass1 == "" or pass2 == "":
-            messages.info(request, "Please enter both the password fields.")
-            return redirect("signup") 
+            # messages.info(request, "Please enter both the password fields.")
+            # return redirect("signup") 
+            context = { "fname" : "", "lname" : "", "email": "", "uname": "", "pass": "Please enter both the password fields."} 
+            return render(request, "signup.html", context)
         else:
             if pass1 == pass2:
                 if User.objects.filter(email = email).exists():
-                    messages.info(request,"User with this Email already exists")
-                    return redirect("signup")
+                    # messages.info(request,"User with this Email already exists")
+                    # return redirect("signup")
+                    context = { "fname" : "", "lname" : "", "email": "User with this Email already exists", "uname": "", "pass": ""}
+                    return render(request, "signup.html", context)
                 elif User.objects.filter(username = uname).exists():
-                    messages.info(request,"User with this Username already exists")
-                    return redirect("signup")
+                    # messages.info(request,"User with this Username already exists")
+                    # return redirect("signup")
+                    context = { "fname" : "", "lname" : "", "email": "", "uname": "User with this Username already exists", "pass": ""}
+                    return render(request, "signup.html", context)
                 else:
                     user = User.objects.create_user(username = uname, email = email, first_name = fname, last_name = lname, password = pass1)
                     user.save();
                     print("Registration completed")
                     return redirect("login")
-                    # messages.info(request,"Registration completed")
-                    # return redirect('signup')
             else:
-                messages.info(request,"Passwords dont match. Please try again")
-                return redirect("signup")
+                # messages.info(request,"Passwords dont match. Please try again")
+                # return redirect("signup")
+                context = { "fname" : "", "lname" : "", "email": "", "uname": "", "pass": "Passwords dont match. Please try again"} 
+                return render(request, "signup.html", context)
 
     else:
         return render(request, 'signup.html')
@@ -54,23 +69,26 @@ def login(request):
     if request.method == "POST":
         uname = request.POST['uname']
         pass1 = request.POST['pass1']
-        
         if uname == "":
-            messages.info(request, "Please enter the User Name")
-            return redirect("login")
+            # messages.info(request, "Please enter your User Name", extra_tags = "empty_uname")
+            # return redirect("login") 
+            context = { "uname" : "Please enter your User Name", "pass" : "", "final": "" }
+            return render(request, "login.html", context)
         elif pass1 == "":
-            messages.info(request, "Please enter the Password")
-            return redirect("login")
+            # messages.info(request, "Please enter the Password", extra_tags = "empty_pass")
+            # return redirect("login")
+            context = { "uname" : "", "pass" : "Please enter your Password", "final": "" }
+            return render(request, "login.html", context)
         else:
-
             user = auth.authenticate(username = uname, password = pass1)
-            
             if user is not None:
                 auth.login(request, user)
                 return redirect("/")
             else:
-                messages.info(request, "Invalid Credentials! Please try again.")
-                return redirect("login")
+                # messages.info(request, "Invalid Credentials! Please try again.", extra_tags = "invalid_creds")
+                # return redirect("login")
+                context = { "uname" : "", "pass" : "", "final": "Invalid Credentials! Please try again." }
+                return render(request, "login.html", context)
 
     else:
         return render(request, "login.html")
