47daf505f0f508d50a5fec62c64eb01d2bd6757a
Muskaan-Nandu <nandumuskaan@gmail.com>
2022-01-21 11:47:06 +0530

    A few css corrections made

--- FNQ/accounts/urls.py
+++ FNQ/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name="index"),
+    path('',views.home, name="home"),
     path("signup", views.signup, name="signup"),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout")
