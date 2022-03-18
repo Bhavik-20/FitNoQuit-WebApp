872b495f0234e31600b51fb79a0cc0573dd3eadc
Bhavik-20 <59500727+Bhavik-20@users.noreply.github.com>
2022-03-18 19:40:10 +0530

    Wo integrated

--- FNQ/accounts/urls.py
+++ FNQ/accounts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
     path("dashboard", views.dashboard, name="dashboard"),
-    path("diet", views.diet, name= "diet")
+    path("diet", views.diet, name = "diet"),
+    path("workout", views.wo_api, name = "workout")
 ]
