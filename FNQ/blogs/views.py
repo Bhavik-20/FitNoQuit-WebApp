36127e4d7c514ca4b1babbaeaae835299b7f2fc6
Muskaan-Nandu <nandumuskaan@gmail.com>
2022-04-14 12:39:33 +0530

    Blogs integration done, css updates remaining

--- FNQ/blogs/views.py
+++ FNQ/blogs/views.py
@@ -11,17 +11,28 @@ def view_blog_categories(request):
     return render(request,"blog-categories.html")
 
 def view_blog_list(request):
-    # if request.method == "POST":
-        #categ = request.POST['category'] #will have to take this from previous page
-    categ = "Weight Loss"
-    get_blogs = Blogs.objects.filter(category=categ)
-    lst_title = []
-    lst_desc = []
-    lst_img =[]
-    for value in get_blogs.all():
-        lst_title.append(value.title)
-        lst_desc.append(value.content)
-        lst_img.append(value.img)
-    dicts = zip(lst_title, lst_desc, lst_img)
-    context={'dicts': dicts, 'category': categ}
-    return render(request, "blog_list.html",context)
+    if request.method == "POST":
+        if 'weight-loss' in request.POST:
+            categ = request.POST['weight-loss']
+        elif 'weight-gain' in request.POST:
+            categ = request.POST['weight-gain']
+        elif 'healthy-lifestyle' in request.POST:
+            categ = request.POST['healthy-lifestyle']
+        elif 'nutrition' in request.POST:
+            categ = request.POST['nutrition']
+        elif 'recipes' in request.POST:
+            categ = request.POST['recipes']
+        elif 'workout' in request.POST:
+            categ = request.POST['workout']
+  
+        get_blogs = Blogs.objects.filter(category=categ)
+        lst_title = []
+        lst_desc = []
+        lst_img =[]
+        for value in get_blogs.all():
+            lst_title.append(value.title)
+            lst_desc.append(value.content)
+            lst_img.append(value.img)
+        dicts = zip(lst_title, lst_desc, lst_img)
+        context={'dicts': dicts, 'category': categ}
+        return render(request, "blog_list.html",context)
