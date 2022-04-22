f205ab8d56352675a64cc11f069c74501b98371c
Bhavik-20 <59500727+Bhavik-20@users.noreply.github.com>
2022-04-22 18:34:16 +0530

    Heroku

--- FNQ/FNQ/settings.py
+++ FNQ/FNQ/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
